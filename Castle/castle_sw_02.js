a681b80b04618e240edfc14e89b9827035606358
ulfenm-code <ulfenm@gmail.com>
2026-09-12 22:16:40 +0200

    Add Castle version 02 service worker

---
+++ Castle/castle_sw_02.js
@@ -0,0 +1,5 @@
+const CACHE='castle-duel-v02';
+const ASSETS=['./castle_index_02.html','./castle_bg.jpg','./castle_manifest_02.webmanifest'];
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));self.skipWaiting();});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE&&k.startsWith('castle-duel-')).map(k=>caches.delete(k)))));self.clients.claim();});
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response;}).catch(()=>caches.match(event.request)));});
